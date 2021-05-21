424b0dec36f83f0599e9cd5a8d2a4bda9fff498f
Nicolas Halberstadt <halberstadtnicolas@gmail.com>
2021-05-21 10:10:17 +0200

    #1 - Hateoas bundle installation

--- config/bundles.php
+++ config/bundles.php
@@ -13,4 +13,5 @@ return [
     Knp\Bundle\PaginatorBundle\KnpPaginatorBundle::class => ['all' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     App\AppBundle\AppBundle::class => ['all' => true],
+    Bazinga\Bundle\HateoasBundle\BazingaHateoasBundle::class => ['all' => true],
 ];
