4079ff7caeddb8c31e5bf2a17070725e42dd79bd
Nicolas Halberstadt <halberstadtnicolas@gmail.com>
2021-05-30 21:16:00 +0200

    #12 - Removing unused function

--- src/Repository/UserRepository.php
+++ src/Repository/UserRepository.php
@@ -18,30 +18,4 @@ class UserRepository extends ServiceEntityRepository
     {
         parent::__construct($registry, User::class);
     }
-    
-    /**
-     * @return User[] Returns an array of User objects
-     */
-    public function findByClient($client): array
-    {
-        return $this->createQueryBuilder('u')
-            ->andWhere('u.client = :client')
-            ->setParameter('client', $client)
-            ->orderBy('u.id', 'ASC')
-            ->setMaxResults(10)
-            ->getQuery()
-            ->getResult();
-    }
-    
-    /*
-    public function findOneBySomeField($value): ?User
-    {
-        return $this->createQueryBuilder('u')
-            ->andWhere('u.exampleField = :val')
-            ->setParameter('val', $value)
-            ->getQuery()
-            ->getOneOrNullResult()
-        ;
-    }
-    */
 }
