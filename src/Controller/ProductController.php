ffc92f4507ea728b98228188e092d71de00a7c7a
Nicolas Halberstadt <halberstadtnicolas@gmail.com>
2021-05-30 21:11:59 +0200

    #4 - Remove product route

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -2,7 +2,9 @@
 
 namespace App\Controller;
 
+use App\AppBundle\Exception\ResourceValidationException;
 use App\Entity\Product;
+use App\Repository\ProductRepository;
 use FOS\RestBundle\Request\ParamFetcherInterface;
 use JMS\Serializer\SerializationContext;
 use JMS\Serializer\SerializerInterface;
@@ -12,6 +14,9 @@ use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use FOS\RestBundle\Controller\Annotations as Rest;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Validator\ConstraintViolationList;
+
 
 class ProductController extends AbstractController
 {
@@ -20,35 +25,16 @@ class ProductController extends AbstractController
      * @var SerializerInterface
      */
     private $serializer;
+    /**
+     * @var ProductRepository
+     */
+    private $productRepository;
     
-    public function __construct(SerializerInterface $serializer)
+    public function __construct(SerializerInterface $serializer, ProductRepository $productRepository)
     {
         $this->serializer = $serializer;
+        $this->productRepository = $productRepository;
     }
-    
-    /**
-     * @Rest\Get(
-     *     path = "/products/{id}",
-     *     name = "app_products_show",
-     *     requirements = {"id"="\d+"}
-     *     )
-     */
-    public function showAction(int $id): Response
-    {
-        /* @var $product Product */
-        $product = $this->getDoctrine()->getRepository(Product::class)->find($id);
-        if ($product == null) {
-            $error = [
-                'code' => 404,
-                'message' => 'No product found with this id',
-            ];
-    
-            return new Response($this->serializer->serialize($error, 'json'), 404);
-        }
-    
-        return new Response($this->serializer->serialize($product, 'json'), 200);
-    }
-    
     
     /**
      * @Rest\Get("/products", name="app_product_list")
@@ -70,7 +56,7 @@ class ProductController extends AbstractController
         ParamFetcherInterface $paramFetcher,
         PaginatorInterface $paginator
     ): Response {
-        $products = $this->getDoctrine()->getRepository(Product::class)->findAll();
+        $products = $this->productRepository->findAll();
         $paginated = $paginator->paginate(
             $products,
             $paramFetcher->get('page'),
@@ -78,5 +64,71 @@ class ProductController extends AbstractController
         );
         
         return new Response($this->serializer->serialize($paginated, 'json'), 200);
+    }
+    
+    /**
+     * @Rest\Get(
+     *     path = "/products/{id}",
+     *     name = "app_products_show",
+     *     requirements = {"id"="\d+"}
+     *     )
+     */
+    public function showAction(int $id): Response
+    {
+        /* @var $product Product */
+        $product = $this->productRepository->find($id);
+        
+        if ($product == null) {
+            throw $this->createNotFoundException("No product found with this id");
+        }
+        
+        return new Response($this->serializer->serialize($product, 'json'), 200);
+    }
+    
+    /**
+     * @Rest\Post(
+     *     path = "/products",
+     *     name = "app_products_create"
+     * )
+     * @Rest\View(StatusCode=201)
+     * @ParamConverter("product", class="App\Entity\Product", converter="fos_rest.request_body")
+     * @throws ResourceValidationException
+     */
+    public function createAction(Product $product, ConstraintViolationList $violations): Response
+    {
+        if (count($violations)) {
+            $message = 'The JSON sent contains invalid data. Here are the errors you need to correct: ';
+            foreach ($violations as $violation) {
+                $message .= sprintf("Field %s: %s ", $violation->getPropertyPath(), $violation->getMessage());
+            }
+            throw new ResourceValidationException($message);
+        }
+        $em = $this->getDoctrine()->getManager();
+        $em->persist($product);
+        $em->flush();
+        
+        return new Response($this->serializer->serialize($product, 'json'), 201);
+    }
+    
+    /**
+     * @Rest\Delete(
+     *    path = "/products/{id}",
+     *    name = "app_products_remove",
+     *    requirements = {"id"="\d+"}
+     * )
+     * @Rest\View(StatusCode=204)
+     */
+    public function removeAction(
+        int $id
+    ): Response {
+        $product = $this->productRepository->find($id);
+        if ($product == null) {
+            throw $this->createNotFoundException("No product found with this id");
+        }
+        $em = $this->getDoctrine()->getManager();
+        $em->remove($product);
+        $em->flush();
+        
+        return new Response(null, 204);
     }
 }
